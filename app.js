a561668361648b64f3f38718ca2c3118bd9af43d
michaelblossom <michaelblossom8654@gmail.com>
2023-04-24 11:52:35 -0700

    product,cart and order contoller added

--- app.js
+++ app.js
@@ -4,6 +4,4 @@ const morgan = require("morgan");
 const app = express();
 app.use(morgan("dev"));
 
-app.use(express.static(`${__dirname}/public`));
-
 module.exports = app;
---
+++ controllers/cartController.js
@@ -0,0 +1,87 @@
+const Cart = require("./../models/cartModel");
+// get allCart
+exports.getAllCarts = async (req, res) => {
+  try {
+    const allCarts = await Cart.find();
+    res.status(200).json({
+      status: "success",
+      data: {
+        carts: allCarts,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "fail",
+      message: error,
+    });
+  }
+};
+// get cart
+exports.getCart = async (req, res) => {
+  try {
+    const cart = await Cart.findById(req.params.id);
+    res.status(200).json({
+      status: "success",
+      data: {
+        carts: cart,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "fail",
+      message: error,
+    });
+  }
+};
+// creating cart
+exports.createCart = async (req, res) => {
+  try {
+    const newCart = await Cart.create(req.body);
+    res.status(201).json({
+      status: "success",
+      data: {
+        cart: newCart,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error,
+    });
+  }
+};
+
+exports.updateCart = async (req, res) => {
+  try {
+    const cart = await Cart.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: "success",
+      data: {
+        cart,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "fail",
+      message: error,
+    });
+  }
+};
+
+exports.deleteCart = async (req, res) => {
+  try {
+    await Cart.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "fail",
+      message: error,
+    });
+  }
+};
