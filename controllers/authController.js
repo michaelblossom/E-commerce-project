25cf17d8a016c7231f4bf2857e524f19ae1b77f0
michaelblossom <michaelblossom8654@gmail.com>
2023-04-27 22:47:17 -0700

    login functionality and middleware to verify authenticated user added

--- controllers/authController.js
+++ controllers/authController.js
@@ -40,6 +40,7 @@ const createAndSendToken = (user, statusCode, res) => {
   });
 };
 
+// signup a user
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
     name: req.body.name,
@@ -50,4 +51,65 @@ exports.signup = catchAsync(async (req, res, next) => {
 
   // calling the createAndSendToken function
   createAndSendToken(newUser, 201, res);
+});
+
+// loggin a user
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  // 1)check if email and password exist
+  if (!email || !password) {
+    return next(new AppError("please provide email and password", 400));
+  }
+
+  // 2)check if user exist and password is correct
+  const user = await User.findOne({ email }).select("+password"); // we are using findone instead of findbyid because we are not using id to find the user rather we are using field(e.g email) also note that we selected the password explicitely because in our model, the select was set to false
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError("incorrect email or password", 401));
+  }
+  //   3)if everything is correct send token
+  //calling createAndSendToken function
+  createAndSendToken(user, 200, res);
+});
+
+// PROTECT MIDDLEWARE
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1)Getting token and check if it exist
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookie.jwt;
+  }
+  if (!token) {
+    // console.log(token);
+    return next(
+      new AppError("you are not logged please login to get access", 401)
+    );
+  }
+  // 2)verify the token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  // console.log(decoded);
+  // 3)check if the user accessing the route still exist
+  const currentUser = await User.findById(decoded.id); //we are using findById because we use our Id as our payload in generating the token that is stored in our decoded
+  if (!currentUser) {
+    return next(
+      new AppError(
+        "The User belonging to this token does not exist anylonger",
+        401
+      )
+    );
+  }
+  // 4)check if user change password after token was issued
+  // calling passwordchangedAfter function from userModel
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError("User recently changed password please login again", 401)
+    );
+  }
+  // GRANT ACCESS TO PROTECTED ROUTE
+  req.user = currentUser;
+  next();
 });
--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -42,5 +44,39 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+// hashing password
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next(); // this line of code simply means that if other fields in the current document such as email, name, etc are modifield excluding the password, the next middleware should run without encripting the password(password should be encripted only when it is modified)
+  this.password = await bcrypt.hash(this.password, 12); //encrypting or hashing the password
+  this.passwordConfirm = undefined; //this will delete password confirm field so that it will not be stored in the database
+  next();
+});
+
+// this query will help us to only get the user that are not deleted(active:true)
+userSchema.pre(/^find/, function (next) {
+  this.find({ active: { $ne: false } });
+  next();
+});
+
+// creating a function that we check if the password that the user entered matched the one stotre stored in the database
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword); //we use compare() function because one of the passwords(userpassword) is encrypted already note that the function will return true or false again candidatepassword is the one that will be provided by the user while userPassword is the one in encrypted in the database
+};
+
+// function to check if the use changed the password after loged in
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  let changedTimestamp;
+  if (this.passwordChangedAt) {
+    changedTimestamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10);
+  }
+  // console.log(this.passwordChangedAt, JWTTimestamp);
+  return JWTTimestamp < changedTimestamp; //100 < 200
+
+  // false means that password not changed
+  return false;
+};
 const User = mongoose.model("User", userSchema);
 module.exports = User;
