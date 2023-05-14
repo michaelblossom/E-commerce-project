a4c96f2b1f620fa1b6f0d5468b0a583bb90d4c77
michaelblossom <michaelblossom8654@gmail.com>
2023-05-14 00:00:32 -0700

    added deleteMe route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -10,6 +10,9 @@ router.post("/login", authController.login);
 // route to get the currently logged user details
 router.get("/me", userController.getMe, userController.getUser);
 
+// route to block/disable user
+router.delete("/deleteMe", userController.deleteMe);
+
 router.route("/").get(userController.getAllUsers);
 
 router.route("/:id").get(userController.getUser);
